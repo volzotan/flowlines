be983db90b7984380315a016d7a6e793a59e8667
Christopher Getschmann <christopher@volzo.de>
2025-03-24 21:46:39 +0100

    bugfixes: python script for test data generation

--- test_data/create_test_data.py
+++ test_data/create_test_data.py
@@ -7,9 +7,17 @@ from scipy import ndimage
 
 
 ELEVATION_FILE = Path("gebco_crop.tif")
+# ELEVATION_FILE = Path("slope_test_2.png")
+
 OUTPUT_PATH = Path(".")
 RESIZE_SIZE = (2000, 2000)
 MIN_INCLINATION = 0.01
+
+BLUR = 20
+
+
+def blur(img: np.ndarray) -> np.ndarray:
+    return cv2.blur(img, (BLUR, BLUR))
 
 
 def normalize_to_uint8(data):
@@ -18,17 +26,21 @@ def normalize_to_uint8(data):
     )
 
 
-def get_slope(data: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+def get_slope(data: np.ndarray, sampling_step: int) -> tuple[np.ndarray, np.ndarray]:
     """
     Computes angle (in rad) and magnitude of the given 2D array of values
     """
-
+    test_slice = data[::sampling_step, ::sampling_step]
     r, c = np.shape(data)
-    Y, X = np.mgrid[0:r, 0:c]
-    dY, dX = np.gradient(data)
+    Y, X = np.mgrid[0:r:sampling_step, 0:c:sampling_step]
+    dY, dX = np.gradient(test_slice)  # order! Y X
 
     angles = np.arctan2(dY, dX)
     magnitude = np.hypot(dY, dX)
+
+    if sampling_step > 1:
+        angles = cv2.resize(angles, data.shape)
+        magnitude = cv2.resize(magnitude, data.shape)
 
     return (angles, magnitude)
 
@@ -40,9 +52,11 @@ with rasterio.open(str(ELEVATION_FILE)) as dataset:
 
 data[data > 0] = 0  # bathymetry data only
 
-angles, inclination = get_slope(data)
+angles, inclination = get_slope(data, 4)
 
 mapping_angle = angles  # float
+mapping_angle += math.pi # center around math.pi (128) so we avoid negative values
+mapping_angle = ((mapping_angle / math.tau)*255).astype(np.uint8)
 
 mapping_non_flat = np.zeros_like(inclination, dtype=np.uint8)
 mapping_non_flat[inclination > MIN_INCLINATION] = 255  # uint8
@@ -61,14 +75,11 @@ win_var = normalize_to_uint8(win_var)
 mapping_max_segments = win_var
 # mapping_max_segments = np.full_like(angles, int(255 / 2))
 
-mapping_angle = cv2.blur(mapping_angle, (10, 10))
-mapping_distance = cv2.blur(mapping_distance, (10, 10))
-mapping_max_segments = cv2.blur(mapping_max_segments, (10, 10))
+mapping_angle = blur(mapping_angle)
+mapping_distance = blur(mapping_distance)
+mapping_max_segments = blur(mapping_max_segments,)
 
-cv2.imwrite(
-    str(Path(OUTPUT_PATH, "map_angle.png")),
-    normalize_to_uint8(mapping_angle / math.tau),
-)
+cv2.imwrite(str(Path(OUTPUT_PATH, "map_angle.png")), mapping_angle)
 cv2.imwrite(str(Path(OUTPUT_PATH, "map_non_flat.png")), mapping_non_flat)
 cv2.imwrite(str(Path(OUTPUT_PATH, "map_distance.png")), mapping_distance)
 cv2.imwrite(str(Path(OUTPUT_PATH, "map_max_segments.png")), mapping_max_segments)
